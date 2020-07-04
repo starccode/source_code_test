050613d1cc360d5a59197f3dbd03e116e6a0f183
Simon <2998769161@qq.com>
2020-07-04 10:22:56 +0800

    ST-commit_test

---
+++ SourceTree/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SourceTree
+//
+//  Created by Simon on 2020/7/4.
+//  Copyright © 2020 Simon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
